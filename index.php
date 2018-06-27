96e2194ef3bf255a977dd3db98e4970a7686f42c
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 19:52:44 -0500

    Pagina de Incio
    Página de Inicio de lomo y parrilla donde podemos encontrar Imágenes e información básica del restaurante

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>SIC</title>
+	<link rel="shortcut icon" type="image/ico" href="images/lom.png">
+	
+<body class="index">
+<!--==============================header=================================-->
+<?php
+include "controlador/include.php";
+?>
+<!--=======content================================-->
+<!--=======galeria================================-->
+<section id="content">
+	<div class="full-width-container block-1">
+		<div class="camera_container">
+			<div id="camera_wrap">
+				<div class="item" data-src="images/comid4.jpg">
+					
+				</div>
+				<div class="item" data-src="images/comid2.jpg">
+					
+				</div>
+				<div class="item" data-src="images/comid3.jpg">
+					
+				</div>
+				<div class="item" data-src="images/comid.jpg">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+<!--=======info================================-->
+	<div class="full-width-container block-2">
+		<div class="container">
+			<div class="row">
+				<div class="grid_12">
+					<header>
+						<h2><span>LOMO & PARRILLA</span></h2>
+					</header>
+					<h4><a href="#">Restaurante y Cafeteria</p>
+					<a href="vista/About.php" class="btn">Informacion</a>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--=======comidas================================-->
+	<div class="full-width-container block-3 parallax-block" data-stellar-background-ratio="0.5">
+		<div class="container">
+			<div class="row">
+				<div class="grid_12">
+				
+					<header>
+						<h2><span>Comidas</span></h2>
+					</header>
+				</div>
+				<div class="row">
+				<div id="owl-carousel" class="owl-carousel">
+					<div class="grid_4">
+						<div class="">
+						
+							<div class="img_container"><a href="vista/menu.php"><img src="images/index_img-1.jpg" alt="img"  height="210"></a></div>
+							
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="">
+							<div class="img_container"><a href="vista/menu.php"><img src="images/index_img-2.jpg" alt="img" height="210"></a></div>
+							
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="">
+							<div class="img_container"><a href="vista/menu.php"><img src="images/index_img-3.jpg" alt="img" height="210"></a></div>
+							
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="">
+							<div class="img_container"><a href="vista/menu.php"><img src="images/costillas_jackdaniels.jpg" alt="" height="210"></a></div>
+							
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="">
+							<div class="img_container"><a href="vista/menu.php"><img src="images/0001.jpg" alt="" height="210"></a></div>
+							
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="">
+							<div class="img_container"><a href="vista/menu.php"><img src="images/1371585527327.jpg" alt="" height="210"></a></div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+	
+
+</section>
+
+<!--=======footer=================================-->
+<footer id="footer">
+	<div class="footer_bottom">
+	<a href="" rel="nofollow"><img src="images/footer-logo.png" alt="logo">
+	</a></div>
+</footer>
+<!--=======footer=================================-->
+<?php
+include "controlador/galeria.php";
+	
+?>
+<!--=======controlador=================================-->
+
+<?php 
+include "controlador/barra_fin_tarea.php";	
+?>
+</body>
+</html>
