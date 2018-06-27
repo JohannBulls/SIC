5023fc6d3335ceabf09306a2b54cf34f352ae3bb
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 19:55:03 -0500

    Barra Fin de la pagina
    La barra fin de la página aparece al final de la página y envía a la página de inicio

---
+++ barra_fin_tarea.php
@@ -0,0 +1,13 @@
+
+<script>
+	$(document).ready(function() { 
+			if ($('php').hasClass('desktop')) {
+				$.stellar({
+					horizontalScrolling: false,
+					verticalOffset: 20,
+					resposive: true,
+					hideDistantElements: true,
+				});
+			}
+		});
+</script>
