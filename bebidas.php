1ad54c8b2850fa8330263ed2eaacd1f3bbbfb7d1
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 21:20:08 -0500

    Bebidas menu
    El código muestra las bebidas en la página principal en el menú del restaurante

---
+++ bebidas.php
@@ -0,0 +1,28 @@
+<div class="adsi-css"><h3><font color="#000000">Bebidas</h3></font></div><br><br>
+<div  class="ceet-css" style="auto"><br><br>
+<!--Platos-->
+<!--Bebidas-->
+<?php
+	if ($gestor = opendir('../platos/Bebidas')) {   
+		echo "<table>";   
+		echo "<tr>";   
+		$i=0;   
+		while (($archivo = readdir($gestor)))   {      
+			if ($archivo!="." && $archivo!="..")      
+			{             
+				if ($i==4)
+				{                
+					$i=0;                   
+					echo "</tr>";                    
+					echo "<tr>";
+				echo "<tr height='30px'>"; echo "<tr>"; echo "<tr>"; 
+			
+				}         
+				$i++;             
+				echo "<td width='30px'>"; echo "<td>"; echo "<td>";           
+				echo "<a href=login.php><img src=../platos/Bebidas/$archivo width='200px' height='200px'> </a>";             
+				echo "</td>";          }   }   
+		echo "</tr>";   echo "</table>";   closedir($gestor); }
+
+?>
+<br><br></div>
