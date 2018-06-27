8866c330f6218c068ad5d5448a281837d2745b34
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 22:45:14 -0500

    Modifica perfil
    Controlador que modifica y envia los datos en la base de datos

---
+++ modificar_perfil.php
@@ -0,0 +1,18 @@
+
+<?php
+
+    session_start();
+    if ($_SESSION['estado']!="1") {
+        header("Location:../controlador/desconectar.php");
+    }
+ require("../controlador/conecxion.php");
+
+ $nombre=($_SESSION['nombre']);
+
+ $mysql->query("update registro set nombre=('$_REQUEST[name]'), apellido=('$_REQUEST[Apellido]'),  Documento=('$_REQUEST[Documento]'), telefono=('$_REQUEST[telefono]'), password=sha1(('$_REQUEST[password]')), pregunta=('$_REQUEST[Pregunta]'), respuesta=('$_REQUEST[Respuesta]') where registro.nombre = '$nombre'") or
+die($mysql->error);
+
+echo"<script type=\"text/javascript\">alert('Datos actulizados con exito'); window.location='perfil.php';</script>"; 
+ 
+ $mysql->close();
+ ?>
