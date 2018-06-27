9f3f036dcc23946415d65d697e0d9c12e1aeae51
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 21:16:45 -0500

    Inhabilita boton derecho
    El codigo Inhabilita la función de el botón derecho del mouse

---
+++ Inhabilitar_boton_derecho.php
@@ -0,0 +1,7 @@
+
+<script>
+// Inhabilitar el botón derecho
+var DADrccolor = "#F1F1F1";
+var DADrcimage = "../images/DENEGADO.png";
+</script>
+<script type="text/javascript" src="http://yourjavascript.com/2122535051/antirightclick.js"></script>
