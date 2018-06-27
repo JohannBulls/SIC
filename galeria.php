9a8970615df4ddb6b249b00ca1c9870c58f72911
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-26 20:01:33 -0500

    El controlador de la galeria
    El controlador de la galería permite mostrar, desplazar las imágenes con respetivo tiempo y estilos

---
+++ galeria.php
@@ -0,0 +1,29 @@
+<script>
+	jQuery(function(){
+		jQuery('#camera_wrap').camera({
+			height: '34.58333333333333%',
+			thumbnails: false,
+			pagination: true,
+			fx: 'simpleFade',
+			loader: 'none',
+			hover: false,
+			navigation: false,
+			playPause: false,
+			minHeight: "139px",
+		});
+	});
+</script>
+<script>
+	$(document).ready(function() {
+		$(".owl-carousel").owlCarousel({
+			navigation: true,
+			pagination: false,
+			items : 3,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,3],
+			itemsTablet: [750,1],
+			itemsMobile : [479,1],
+			navigationText: false
+		});
+	});
+</script>
