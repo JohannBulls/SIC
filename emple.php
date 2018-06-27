9ef5154b5c0740e751219e136a3422d1cbabac6b
lomoparrilla <40411821+lomoparrilla@users.noreply.github.com>
2018-06-27 06:49:48 -0500

    Empleado
    pagina empleado

---
+++ emple.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>About</title>
+<link rel="shortcut icon" type="../image/ico" href="../images/lom.png">
+	
+	</head>
+<body class="index-1">
+<!--==============================header=================================-->
+<?php
+include "include_emple.php";
+?>
+<!--=======content================================-->
+
+<section id="content">
+	<div class="full-width-container block-1">
+		<div class="container">
+			<div class="row">
+				<center><header>
+						<h11><span>NOSOTROS</span></h11>
+					</header></center><br><br>
+					
+				
+				<div class="grid_4">
+					<div class="img_container"><br><br><img src="../images/index-1_img-1.png" alt="img" width="5" height="290"></div>
+				</div>
+				<div class="grid_6 preffix_1">
+				    <center><h8><span>VISIÓN</span></h8></center>
+				
+	<p>Ser un  Restaurante, líder de tradición a nivel nacional, regional y mundial de constante innovación en recetas caseras buscando las más exquisitas combinaciones para deleitar el paladar de nuestros clientes.
+</p>
+<br>
+
+<center><h8><span>MISIÓN</span></h8></center>
+	<p>Satisfacer las necesidades gastronómicas de nuestros clientes, ofreciendo alimentos y servicios con la más alta calidad, donde se sobrepasen las expectativas de nuestros clientes y ser un espacio de trabajo que permita la realización personal y el Desarrollo de sus colaboradores en el grupo.
+Tomando como estrategia principal el mejoramiento continúo de los estrictos estándares de higiene y buen servicio en un ambiente seguro, agradable y familiar a un precio justo.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="full-width-container block-2 parallax-block" data-stellar-background-ratio="0.5">
+	<center>
+						<h11><span style="color:#FFFFFF">Nuestro Equipo</span></h11>
+					</center><br><br>
+		<center>
+			<div class="row">
+				<div class="grid_12" >
+					
+						
+						
+					
+				</div><br><br>
+				<br><br>
+				
+			<?php include "../vista/info_empleados.php";?>	
+			
+		</center>
+	</div></div>
+	
+</section>
+
+<!--=======footer=================================-->
+<footer id="footer">
+	<div class="footer_bottom">
+	<a href="" rel="nofollow"><img src="../images/footer-logo.png" alt="logo">
+	</a></div>
+</footer>
+<!--=======controlador=================================-->
+
+<?php
+
+include "../controlador/barra_fin_tarea.php";	
+?>
+</body>
+</html>
